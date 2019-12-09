e6f629cfa26f89b2e31e9b0196c8a3836f5fd4cb
Aston <astonoturner@gmail.com>
2019-12-09 14:15:40 +0000

    Initial Commit

---
+++ Aston-Coursework/Aston-Coursework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aston-Coursework
+//
+//  Created by Aston on 09/12/2019.
+//  Copyright © 2019 16052488. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
