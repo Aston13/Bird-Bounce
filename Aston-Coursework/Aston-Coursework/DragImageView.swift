57c078cfd858aba655b71b12b2c2b47fed71a25c
Aston <astonoturner@gmail.com>
2020-01-03 13:32:31 +0100

    Added gravity, collisions and linear velocity

--- Aston-Coursework/Aston-Coursework/DragImageView.swift
+++ Aston-Coursework/Aston-Coursework/DragImageView.swift
@@ -12,10 +12,8 @@ class DragImageView: UIImageView {
     
     /* Original starting location used by touchesEnded() to reset crosshair */
     var spawnPoint = CGPoint(x: maxNotch + crosshairSize/2,y: screenHeight/2)
-    
+    var myBallDelegate: ballViewDelegate?
     var startLocation: CGPoint? // Starting location used by touchesMoved()
-    var myDelegate: crosshairViewDelegate?;
-
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         startLocation = touches.first?.location(in: self)
@@ -28,7 +26,7 @@ class DragImageView: UIImageView {
         let dy = currentLocation!.y - startLocation!.y
         var newCenter = CGPoint(x: self.center.x+dx, y: self.center.y+dy)
         
-        /* Constrain the movement of the crosshair.
+        /* Restrict the movement bounds of the crosshair.
          * Comments are in perspective to the top-left corner of the screen (x0,y0).
          */
 
@@ -46,5 +44,6 @@ class DragImageView: UIImageView {
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.center = spawnPoint
+        self.myBallDelegate?.shoot()
     }
 }
--- Aston-Coursework/Aston-Coursework/ViewController.swift
+++ Aston-Coursework/Aston-Coursework/ViewController.swift
@@ -8,36 +8,93 @@
     
 import UIKit
 
-// Global variables
-public var maxNotch: CGFloat = 35;
-public var crosshairSize: CGFloat = 50;
-public var screenWidth = UIScreen.main.bounds.width - maxNotch
+/* Global variables */
+public var maxNotch: CGFloat = 35; // Maximum thickness of the notch area on an iPhone
+public var crosshairSize: CGFloat = 50; // Crosshair size - square - even x and y dimensions
+public var ballSize: CGFloat = 50; // Ball size - square - even x and y dimensions
+public var screenWidth = UIScreen.main.bounds.width - maxNotch // Overall screen width - not including notch
 public var screenHeight = UIScreen.main.bounds.height
 
-protocol crosshairViewDelegate {
-    
+var shotBalls = [UIDynamicItem]() // Delaration of an array to store UIDynamicItem objects
+
+/* Protocols for delegated functions */
+protocol ballViewDelegate {
+    func shoot()
 }
 
-class ViewController: UIViewController, crosshairViewDelegate {
+class ViewController: UIViewController, ballViewDelegate {
 
-    @IBOutlet weak var crosshairImageView: DragImageView!
+    @IBOutlet weak var crosshairImageView: DragImageView! // Crosshair image outlet reference
     
+    var dynamicAnimator: UIDynamicAnimator!
+    var dynamicBehavior: UIDynamicItemBehavior! // Declare dynamic behaviour
+    var collisionBehavior: UICollisionBehavior! // Declare collisions
+    var gravityBehavior: UIGravityBehavior!     // Declare gravity
+    
+    /* Function to setup and add a crosshair to the subview with an image,
+     * starting location and dimensions.
+     */
     func initialiseCrosshair() {
         crosshairImageView.image = UIImage(named: "aim.png")
         crosshairImageView.frame = CGRect(x: maxNotch, y: (screenHeight/2) - (crosshairSize/2), width: crosshairSize, height: crosshairSize)
         self.view.addSubview(crosshairImageView)
     }
     
+    /* Function called by touchesEnded() in DragImageView that 'shoots' a ball. The function creates a new
+     * UIImage Ball, adds it to the subview and the ball array and then UIDynamics is implemented.
+     */
+    func shoot() {
+        /* Create a ball and add it to the subview */
+        let shotBall = UIImageView(image: nil)
+        shotBall.image = UIImage(named: "ball.png")
+        shotBall.frame = CGRect(x: maxNotch, y: (screenHeight/2 - crosshairSize/2), width: ballSize, height: ballSize)
+        self.view.addSubview(shotBall)
+        
+        shotBalls.append(shotBall) // Add the newly created ball that has been shot to the balls array
+        
+        /* Add behaviours to the ball objects */
+        /* Dynamic Item Behaviour */
+        dynamicBehavior = UIDynamicItemBehavior(items: shotBalls) // Add balls to the dynamic item behaviour
+        dynamicAnimator.addBehavior(dynamicBehavior) // Add behaviour to the animator
+        
+        
+        /* Gravity Behaviour */
+        gravityBehavior = UIGravityBehavior(items: shotBalls)
+        dynamicAnimator.addBehavior(gravityBehavior)
+        
+        /* Collision Behaviour */
+        collisionBehavior = UICollisionBehavior(items: shotBalls)
+        dynamicAnimator.addBehavior(collisionBehavior)
+        
+        
+        
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x:500, y:0), for: shotBall) // Speed and direction
+        self.gravityBehavior.magnitude = 0.2 // Gravitational force
+        self.collisionBehavior.translatesReferenceBoundsIntoBoundary = true // Collision boundaries
+        //self.collisionBehavior.removeBoundary(withIdentifier: )
+        
+    }
+    
+    /* Intialise and setup */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        crosshairImageView.myDelegate = self
+        /* Passes the main view to dynamics as the reference view */
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        crosshairImageView.myBallDelegate = self
+        
+        /* Orientation Initialisation */
         let value = UIInterfaceOrientation.landscapeLeft.rawValue
         UIDevice.current.setValue(value, forKey: "orientation")
+        
         initialiseCrosshair()
     }
     
+    
+    
+    
+    /* Functions to force orientation and autorotate */
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscape
     }
