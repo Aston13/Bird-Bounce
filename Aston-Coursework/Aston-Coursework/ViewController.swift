8264ae6f1e7e9d74257088a73079b6fa6d47b4f5
Aston <astonoturner@gmail.com>
2020-01-04 22:24:32 +0100

    Code and comments cleanup

--- Aston-Coursework/Aston-Coursework/ViewController.swift
+++ Aston-Coursework/Aston-Coursework/ViewController.swift
@@ -5,43 +5,18 @@
 //  Created by Aston on 09/12/2019.
 //  Copyright © 2019 16052488. All rights reserved.
 //
-    
+
 import UIKit
 
 /* Global variables */
-public var maxNotch: CGFloat = 35 // Maximum thickness of the notch area on an iPhone
-public var crosshairSize: CGFloat = 50 // Crosshair size - square - even x and y dimensions
-public var ballSize: CGFloat = 50; // Ball size - square - even x and y dimensions
-public var screenWidth = UIScreen.main.bounds.width - maxNotch // Overall screen width - not including notch
-public var screenHeight = UIScreen.main.bounds.height
-public var birdSize: CGFloat = (screenHeight-30)/5
-
-var bird = UIImageView(image:nil)
-var birds = [UIImageView]()
-var shotBall = UIImageView(image: nil)
-var shotBalls = [UIImageView]() // Delaration of an array to store UIDynamicItem objects
-var obstacleArray = [UIImageView]()
-let scoreLabel = UILabel()
-var totalScore: Int = 0
-
-var birdPositions: [Int] = [0,0,0,0,0]
-
-var positionsArray: [CGRect] = [
-    CGRect(x:(screenWidth - maxNotch) - (birdSize/2 + 5), y: 5, width: birdSize, height: birdSize),
-    
-    CGRect(x:(screenWidth - maxNotch) - (birdSize/2 + 5), y: 10 + birdSize, width: birdSize, height: birdSize),
-    
-    CGRect(x:(screenWidth - maxNotch) - (birdSize/2 + 5), y: 15 + (birdSize*2), width: birdSize, height: birdSize),
-    
-    CGRect(x:(screenWidth - maxNotch) - (birdSize/2 + 5), y: 20 + (birdSize*3), width: birdSize, height: birdSize),
-    
-    CGRect(x:(screenWidth - maxNotch) - (birdSize/2 + 5), y: 25 + (birdSize*4), width: birdSize, height: birdSize)]
-
-public var crosshairVectorXY = CGPoint(x:0,y:0)
-
-
-
-
+/* Dimensions */
+let maxNotch: CGFloat = 35 // Maximum thickness of the notch area on an iPhone
+let screenWidth = UIScreen.main.bounds.width - maxNotch // Overall screen width - not including notch
+let screenHeight = UIScreen.main.bounds.height
+let crosshairSize: CGFloat = 50 // Crosshair size - square - even x and y dimensions
+var crosshairVectorXY = CGPoint(x:0,y:0) // Vector used to store and retrieve the angle of the shot fired
+let birdSize: CGFloat = (screenHeight-30)/5
+let ballSize: CGFloat = 50; // Ball size - square - even x and y dimensions
 
 /* Protocols for delegated functions */
 protocol ballViewDelegate {
@@ -49,13 +24,78 @@ protocol ballViewDelegate {
 }
 
 class ViewController: UIViewController, ballViewDelegate {
+    
+    /* Ball storage */
+    var shotBall = UIImageView(image: nil)
+    var shotBalls = [UIImageView]()
+
+    /* Bird storage */
+    var bird = UIImageView(image:nil)
+    var birds = [UIImageView]()
+    var birdPositions: [Int] = [0,0,0,0,0] // Bird position slots TR -> BR. 0 = Empty. 1 = Occupied.
+
+    /* 5 possible bird co-ordinate positions TR -> BR */
+    let positionsArray: [CGRect] = [
+        CGRect(x:(screenWidth - maxNotch) - (birdSize/2 + 5), y: 5, width: birdSize, height: birdSize),
+        CGRect(x:(screenWidth - maxNotch) - (birdSize/2 + 5), y: 10 + birdSize, width: birdSize, height: birdSize),
+        CGRect(x:(screenWidth - maxNotch) - (birdSize/2 + 5), y: 15 + (birdSize*2), width: birdSize, height: birdSize),
+        CGRect(x:(screenWidth - maxNotch) - (birdSize/2 + 5), y: 20 + (birdSize*3), width: birdSize, height: birdSize),
+        CGRect(x:(screenWidth - maxNotch) - (birdSize/2 + 5), y: 25 + (birdSize*4), width: birdSize, height: birdSize)]
+
+    /* UI */
+    let scoreLabel = UILabel()
+    var totalScore: Int = 0
+    
+    /* Behavioural variables */
+    var dynamicAnimator: UIDynamicAnimator! // Physics Engine
+    var dynamicBehavior: UIDynamicItemBehavior!
+    var collisionBehavior: UICollisionBehavior!
+    var gravityBehavior: UIGravityBehavior!
 
     @IBOutlet weak var crosshairImageView: DragImageView! // Crosshair image outlet reference
     
-    var dynamicAnimator: UIDynamicAnimator!     // Physics Engine
-    var dynamicBehavior: UIDynamicItemBehavior! // Declare dynamic behaviour
-    var collisionBehavior: UICollisionBehavior! // Declare collisions
-    var gravityBehavior: UIGravityBehavior!     // Declare gravity
+    /* Setup */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        /* Passes the main view to dynamics as the reference view. */
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        /* Checks every x seconds and adds a random bird to a random empty slot. */
+        _ = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block:{_ in
+            self.checkEmptyBirdPositions()
+        })
+        
+        /* Orientation Initialisation */
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+        
+        crosshairImageView.myBallDelegate = self
+        initialiseCrosshair()
+        initialiseScoreLabel()
+    }
+    
+    /* Function to force orientation to landscape. */
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+    
+    /* Function to allow autorotate of orientation. */
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    func initialiseScoreLabel() {
+        scoreLabel.frame = CGRect(x:maxNotch, y:10, width:100, height:20)
+        scoreLabel.textAlignment = NSTextAlignment.left
+        scoreLabel.text = "Score: \(totalScore)"
+        self.view.addSubview(scoreLabel)
+    }
+    
+    func increaseScore(score:Int) {
+        totalScore = totalScore + score
+        scoreLabel.text = "Score: \(totalScore)"
+    }
     
     /* Function to setup and add a crosshair to the subview with an image,
      * starting location and dimensions.
@@ -66,49 +106,32 @@ class ViewController: UIViewController, ballViewDelegate {
         self.view.addSubview(crosshairImageView)
     }
     
+    /* Checks a random bird slot (0-4) in birdPositions. If slot is empty,
+     * then a function to add a random bird to that slot is called.
+     */
     func checkEmptyBirdPositions() {
-        
-        let randomSlotNum: Int = Int(arc4random_uniform(5))
+        let randomSlotNum: Int = Int(arc4random_uniform(5)) // Random number 0-4
         
         if birdPositions[randomSlotNum] == 0 { // If slot is empty
             addBird(position: positionsArray[randomSlotNum], slotNum: randomSlotNum) // Add bird to slot
-                birdPositions[randomSlotNum] = 1
+                birdPositions[randomSlotNum] = 1 // Set slot to occupied (1)
         }
     }
     
-//    func clearSlot(deadSlot: Int) {
-//        var count: Int = 0
-//        var count:CGPoint = CGPoint(deadSlot.midX)
-//        for slot in positionsArray {
-//            if slot.equalTo(deadSlot) {
-//                birdPositions[count] = 0
-//                break;
-//            }
-//            count+=1
-//        }
-//
-//
-//    }
-    
+    /* Adds a random bird in the position (0-4) passed.
+     * slotNum is used as an identifaction tag to track the slot position.
+     */
     func addBird(position: CGRect, slotNum: Int) {
-
-        
+  
+        /* Create a new bird object */
         bird = UIImageView(image: nil)
-        bird.image = UIImage(named: "bird\(arc4random_uniform(12) + 1)")
-        bird.frame = position
-        bird.tag = slotNum
-        //bird.backgroundColor = UIColor.red
-        self.view.addSubview(bird)
-
-        birds.append(bird)
-
-//        dynamicBehavior = UIDynamicItemBehavior(items: [bird])
-//        dynamicAnimator.addBehavior(dynamicBehavior)
-//
-//        collisionBehavior = UICollisionBehavior(items: [bird])
-//        //collisionBehavior = UICollisionBehavior(items: birds)
-//        dynamicAnimator.addBehavior(collisionBehavior)
         
+        // Loads a bird image at random from 1-12
+        bird.image = UIImage(named: "bird\(arc4random_uniform(12) + 1)")
+        bird.frame = position   // Pre-set CGRect position
+        bird.tag = slotNum      // ID Number relating to position
+        self.view.addSubview(bird)
+        birds.append(bird)      // Add to birds[] array
     }
     
     /* Function called by touchesEnded() in DragImageView that 'shoots' a ball. The function creates a new
@@ -120,42 +143,36 @@ class ViewController: UIViewController, ballViewDelegate {
         shotBall = UIImageView(image: nil)
         shotBall.image = UIImage(named: "ball.png")
         shotBall.frame = CGRect(x: maxNotch, y: (screenHeight/2 - crosshairSize/2), width: ballSize, height: ballSize)
-        //shotBall.backgroundColor = UIColor.blue
         self.view.addSubview(shotBall)
-        
-        shotBalls.append(shotBall) // Add the newly created ball that has been shot to the shotBalls array
+        shotBalls.append(shotBall) // Add newly created ball to shotBalls[] array
         
         /* Behaviours */
-        
+
         /* Dynamic Item Behaviour */
-        dynamicBehavior = UIDynamicItemBehavior(items: [shotBall]) // Add balls to the dynamic item behaviour
-        
-        // Speed and direction
-        self.dynamicBehavior.addLinearVelocity(CGPoint(x:crosshairVectorXY.x, y:crosshairVectorXY.y), for: shotBall)
-        
-        dynamicAnimator.addBehavior(dynamicBehavior) // Add dynamic item to animator
+        dynamicBehavior = UIDynamicItemBehavior(items: [shotBall])
+        self.dynamicBehavior.addLinearVelocity(CGPoint(x:crosshairVectorXY.x, y:crosshairVectorXY.y), for: shotBall)      // Speed and direction
+        dynamicAnimator.addBehavior(dynamicBehavior)    // Add dynamicItem to animator
 
         /* Gravity Behaviour */
         gravityBehavior = UIGravityBehavior(items: [shotBall])
-        self.gravityBehavior.magnitude = 0.2 // Gravitational force
-        
-        dynamicAnimator.addBehavior(gravityBehavior) // Add gravity to animator
+        self.gravityBehavior.magnitude = 0.2            // Gravitational force
+        dynamicAnimator.addBehavior(gravityBehavior)    // Add gravity to animator
 
         /* Collision Behaviour */
         collisionBehavior = UICollisionBehavior(items: shotBalls)
+        dynamicAnimator.addBehavior(collisionBehavior)  // Add collision to animator
         
         /* Passively checks for collision between ball and bird */
         collisionBehavior.action = {
-            for itema in shotBalls {
-                for itemb in birds {
-                    if itema.frame.intersects(itemb.frame) {
+            for itemA in self.shotBalls {
+                for itemB in self.birds {
+                    if itemA.frame.intersects(itemB.frame) {
+                        let preSubviewAmount = self.view.subviews.count
+                        itemB.removeFromSuperview()
+                        let postSubviewAmount = self.view.subviews.count
                         
-                        let before = self.view.subviews.count
-                        itemb.removeFromSuperview()
-                        let after = self.view.subviews.count
-                        
-                        if (before != after){
-                            birdPositions[Int(itemb.tag)] = 0
+                        if (preSubviewAmount != postSubviewAmount){
+                            self.birdPositions[Int(itemB.tag)] = 0
                             self.increaseScore(score: 1)
                         }
                     }
@@ -163,66 +180,17 @@ class ViewController: UIViewController, ballViewDelegate {
             }
         }
         
-        /* Despawn balls after 5 seconds - frees up memory */
+        /* Despawn balls after 5 seconds - prevents memory leak. */
         _ = Timer.scheduledTimer(withTimeInterval: 5, repeats: false, block:{_ in
-            if shotBalls.isEmpty == false {
-                shotBalls[0].removeFromSuperview()
-                shotBalls.removeFirst()
+            if self.shotBalls.isEmpty == false {
+                self.shotBalls[0].removeFromSuperview()
+                self.shotBalls.removeFirst()
             }
         })
         
-        // Collision Boundaries - left, top and bottom sides of the screen
+        /* Collision Boundaries - left, top and bottom sides of the screen. */
         self.collisionBehavior.addBoundary(withIdentifier: "leftBoundary" as NSCopying, from: CGPoint(x:0, y:0), to: CGPoint(x:0, y:screenHeight))
         self.collisionBehavior.addBoundary(withIdentifier: "topBoundary" as NSCopying, from: CGPoint(x:0, y:0), to: CGPoint(x: screenWidth + maxNotch, y: 0))
         self.collisionBehavior.addBoundary(withIdentifier: "bottomBoundary" as NSCopying, from: CGPoint(x:0, y:screenHeight), to: CGPoint(x: screenWidth + maxNotch, y: screenHeight))
-        
-        dynamicAnimator.addBehavior(collisionBehavior) // Add collision to animator
     }
-    
-    func initialiseScoreLabel() {
-        scoreLabel.frame = CGRect(x:maxNotch, y:10, width:100, height:20)
-        scoreLabel.textAlignment = NSTextAlignment.left
-        scoreLabel.text = "Score: \(totalScore)"
-        self.view.addSubview(scoreLabel)
-        
-    }
-    
-    func increaseScore(score:Int) {
-        totalScore = totalScore + score
-        scoreLabel.text = "Score: \(totalScore)"
-    }
-    
-    /* Intialise and setup */
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-        
-        /* Passes the main view to dynamics as the reference view */
-        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
-        
-        _ = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block:{_ in
-            self.checkEmptyBirdPositions()
-        })
-        crosshairImageView.myBallDelegate = self
-    
-        /* Orientation Initialisation */
-        let value = UIInterfaceOrientation.landscapeLeft.rawValue
-        UIDevice.current.setValue(value, forKey: "orientation")
-        
-
-        initialiseCrosshair()
-        initialiseScoreLabel()
-
-    }
-    
-    /* Functions to force orientation and autorotate */
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .landscape
-    }
-    
-    override var shouldAutorotate: Bool {
-        return true
-    }
-    
 }
